c1c1fe9b463238c2c5d18a5102d150b7233affed
Leo Graciano <leonardoferreiragraciano@gmail.com>
2024-06-09 15:38:31 -0300

    feat: Adding class SearchParams

--- src/shared/repositories/searchable-repository-contracts.ts
+++ src/shared/repositories/searchable-repository-contracts.ts
@@ -1,6 +1,57 @@
 import { Entity } from '../domain/entities/entity'
 import { RepositoryInterface } from './repository-contracts'
 
+export type SortDirection = 'asc' | 'desc'
+
+export type SearchProps<Filter = string> = {
+  page?: number
+  perPage?: number
+  sort?: string | null
+  sortDirection?: SortDirection | null
+  filter?: Filter | null
+}
+
+export class SearchParams {
+  protected _page: number
+  protected _perPage: number
+  protected _sort: string | null
+  protected _sortDirection: SortDirection | null
+  protected _filter: string | null
+
+  constructor(props: SearchProps) {
+    this._page = props.page ?? 1
+    this._perPage = props.perPage ?? 15
+    this._sort = props.sort ?? null
+    this._sortDirection = props.sortDirection ?? null
+    this._filter = props.filter ?? null
+  }
+
+  get page(): number {
+    return this._page
+  }
+  private set page(value: number) {}
+
+  get perPage(): number {
+    return this._perPage
+  }
+  private set perPage(value: number) {}
+
+  get sort(): string | null {
+    return this._sort
+  }
+  private set sort(value: string | null) {}
+
+  get sortDirection(): SortDirection | null {
+    return this._sortDirection
+  }
+  private set sortDirection(value: SortDirection | null) {}
+
+  get filter(): string | null {
+    return this._filter
+  }
+  private set filter(value: string | null) {}
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
   SearchInput,

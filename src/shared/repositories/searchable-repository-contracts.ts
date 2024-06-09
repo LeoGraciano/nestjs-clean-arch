6d8552aec070f23c23218e9b00e0a7d5ea5c6528
Leo Graciano <leonardoferreiragraciano@gmail.com>
2024-06-09 15:54:15 -0300

    feat: add setters in class SearchParams

--- src/shared/repositories/searchable-repository-contracts.ts
+++ src/shared/repositories/searchable-repository-contracts.ts
@@ -22,34 +22,63 @@ export class SearchParams {
     this._page = props.page ?? 1
     this._perPage = props.perPage ?? 15
     this._sort = props.sort ?? null
-    this._sortDirection = props.sortDirection ?? null
+    this._sortDirection = props.sortDirection ?? 'desc'
     this._filter = props.filter ?? null
   }
 
   get page(): number {
     return this._page
   }
-  private set page(value: number) {}
+  private set page(value: number) {
+    let _page = +value
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+      _page = 1
+    }
+    this._page = _page
+  }
 
   get perPage(): number {
     return this._perPage
   }
-  private set perPage(value: number) {}
+  private set perPage(value: number) {
+    let _perPage = +value
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      parseInt(_perPage as any) !== _perPage
+    ) {
+      _perPage = this._perPage
+    }
+    this._perPage = _perPage
+  }
 
   get sort(): string | null {
     return this._sort
   }
-  private set sort(value: string | null) {}
+  private set sort(value: string | null) {
+    this._sort =
+      value === null || value === undefined || value === '' ? null : `${value}`
+  }
 
   get sortDirection(): SortDirection | null {
     return this._sortDirection
   }
-  private set sortDirection(value: SortDirection | null) {}
+  private set sortDirection(value: SortDirection | null) {
+    if (!this.sort) {
+      this._sortDirection = null
+      return
+    }
+    const dir = `${value}`.toLowerCase()
+    this._sortDirection = dir !== 'asc' ? this._sortDirection : `${value}`
+  }
 
   get filter(): string | null {
     return this._filter
   }
-  private set filter(value: string | null) {}
+  private set filter(value: string | null) {
+    this._filter =
+      value === null || value === undefined || value === '' ? null : `${value}`
+  }
 }
 
 export interface SearchableRepositoryInterface<

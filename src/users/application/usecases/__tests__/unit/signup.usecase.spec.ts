a88dfdaef35431a063d594990f83e21f32805af3
Leo Graciano <leonardoferreiragraciano@gmail.com>
2024-06-12 19:54:45 -0300

    test: SignupUseCase unit test

---
+++ src/users/application/usecases/__tests__/unit/signup.usecase.spec.ts
@@ -0,0 +1,53 @@
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/reposotires/user-in-memory.repository'
+import { SignupUseCase } from '../../signup.usecase'
+import { HashProvider } from '@/shared/application/providers/hash-provider'
+import { BcryptjsHashProvider } from '@/users/infrastructure/providers/hash-provider/bcryptjs-hash.provider'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data.builder'
+import { ConflictError } from '@/shared/domain/errors/conflict-error'
+import { BadRequestError } from '@/users/application/errors/bad-request-error'
+
+describe('SignupUseCase', () => {
+  let sut: SignupUseCase.UseCase
+  let repository: UserInMemoryRepository
+  let hashProvider: HashProvider
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    hashProvider = new BcryptjsHashProvider()
+    sut = new SignupUseCase.UseCase(repository, hashProvider)
+  })
+  it('Should create a user', async () => {
+    const sypInsert = jest.spyOn(repository, 'insert')
+    const props = UserDataBuilder({})
+    const result = await sut.execute({
+      name: props.name,
+      email: props.email,
+      password: props.password,
+    })
+    expect(result.id).toBeDefined()
+    expect(sypInsert).toHaveBeenCalled()
+    expect(result.name).toEqual(props.name)
+    expect(props.createdAt).toBeInstanceOf(Date)
+    expect(sypInsert).toHaveBeenCalledTimes(1)
+  })
+  it('Should not be able to register with same email twice', async () => {
+    const props = UserDataBuilder({ email: 'a@a.com' })
+    await sut.execute(props)
+    expect(() => sut.execute(props)).rejects.toBeInstanceOf(ConflictError)
+  })
+  it('Should throws error when name not provided', async () => {
+    const props = Object.assign(UserDataBuilder({}), { name: null })
+
+    expect(() => sut.execute(props)).rejects.toBeInstanceOf(BadRequestError)
+  })
+  it('Should throws error when email not provided', async () => {
+    const props = Object.assign(UserDataBuilder({}), { email: null })
+
+    expect(() => sut.execute(props)).rejects.toBeInstanceOf(BadRequestError)
+  })
+  it('Should throws error when password not provided', async () => {
+    const props = Object.assign(UserDataBuilder({}), { password: null })
+
+    expect(() => sut.execute(props)).rejects.toBeInstanceOf(BadRequestError)
+  })
+})

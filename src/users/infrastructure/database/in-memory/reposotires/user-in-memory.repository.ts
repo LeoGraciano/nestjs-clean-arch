6d20ca866d552c08f35502d5e01b63e7d1e3d677
Leo Graciano <leonardoferreiragraciano@gmail.com>
2024-06-12 14:47:02 -0300

    refactor: user repository  - part 2

--- src/users/infrastructure/database/in-memory/reposotires/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/reposotires/user-in-memory.repository.ts
@@ -1,13 +1,15 @@
 import { ConflictError } from '@/shared/domain/errors/conflict-error'
 import { NotFoundError } from '@/shared/domain/errors/not-found-error'
 import { InMemorySearchableRepository } from '@/shared/repositories/in-memory-searchable.repository'
+import { SortDirection } from '@/shared/repositories/searchable-repository'
 import { UserEntity } from '@/users/domain/entities/user.entity'
-import { UserRepositoryInterface } from '@/users/domain/repositories/user.repository'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepositoryInterface
+  implements UserRepository.Repository
 {
+  sortableFields: string[] = ['name', 'createdAt']
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find(item => item.email === email)
     if (!entity) {
@@ -20,5 +22,24 @@ export class UserInMemoryRepository
     if (entity) {
       throw new ConflictError(`Email address in used ${email}`)
     }
+  }
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: UserRepository.Filter,
+  ): Promise<UserEntity[]> {
+    if (!filter) return items
+    return items.filter(item =>
+      item.props.name.toLowerCase().includes(filter.toLowerCase()),
+    )
+  }
+
+  protected async applySort(
+    items: UserEntity[],
+    sort: string | null,
+    sortDirection: SortDirection | null,
+  ): Promise<UserEntity[]> {
+    return !sort
+      ? super.applySort(items, 'createdAt', 'desc')
+      : super.applySort(items, sort, sortDirection)
   }
 }

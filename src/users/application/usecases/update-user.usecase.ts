18c30323222ed108a93be8b9bcc16f6c3c1fda52
Leo Graciano <leonardoferreiragraciano@gmail.com>
2024-06-14 10:00:15 -0300

    feat: add class UpdateUserUseCase

--- src/users/application/usecases/update-user.usecase.ts
+++ src/users/application/usecases/update-user.usecase.ts
@@ -1,17 +1,23 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { UserOutput, UserOutputMapper } from '../dtos/user-output'
 import { UseCase as DefaultUseCase } from '@/shared/application/use-cases'
-export namespace GetUserUseCase {
+import { BadRequestError } from '@/shared/application/errors/bad-request-error'
+export namespace UpdateUserUseCase {
   export type Input = {
     id: string
+    name: string
   }
 
   export type Output = UserOutput
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
+
     async execute(input: Input): Promise<Output> {
+      if (!input.name) throw new BadRequestError('name not provided')
       const entity = await this.userRepository.findById(input.id)
+      entity.update(input.name)
+      await this.userRepository.update(entity)
       return UserOutputMapper.toOutput(entity)
     }
   }
